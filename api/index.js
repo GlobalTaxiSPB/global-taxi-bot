bed78332900bef55392ad8b18d2dafc9744d3929
GlobalTaxiSPB <yamburgmedia@gmail.com>
2026-07-17 23:51:22 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -41,8 +41,9 @@ export default async function handler(req, res) {
     };
   }
 
-  ) {
+  else if (text === "🚖 Заказать поездку") {
     message = "📍 Напишите адрес подачи автомобиля.";
+  }
   }
 
   else if (text === "💰 Рассчитать стоимость") {
