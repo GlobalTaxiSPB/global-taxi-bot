1ae8173c5b36a4e7de69dfd4d348ed86c5c48957
GlobalTaxiSPB <yamburgmedia@gmail.com>
2026-07-17 23:02:06 +0300

    Update state.js

--- api/state.js
+++ api/state.js
@@ -1,1 +1,16 @@
+const state = new Map();
 
+export function getState(chatId) {
+  return state.get(chatId) || {};
+}
+
+export function setState(chatId, data) {
+  state.set(chatId, {
+    ...getState(chatId),
+    ...data,
+  });
+}
+
+export function clearState(chatId) {
+  state.delete(chatId);
+}
