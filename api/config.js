31751bbd0b0715815cc01b1811c4c9ace83221c4
GlobalTaxiSPB <yamburgmedia@gmail.com>
2026-07-17 22:26:15 +0300

    Create config.js

---
+++ api/config.js
@@ -0,0 +1,13 @@
+// Global Taxi SPB
+// Конфигурация проекта
+
+module.exports = {
+  BOT_NAME: "Global Taxi SPB",
+  BOT_VERSION: "1.0.0",
+
+  ADMIN_ID: process.env.ADMIN_ID,
+  BOT_TOKEN: process.env.BOT_TOKEN,
+
+  ORDER_PREFIX: "GT",
+  TIMEZONE: "Europe/Moscow"
+};
